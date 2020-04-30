2f594430f353a83b3509211b4b85c2cc4d6f7b5e
dawiddd95 <dawlyc@wp.pl>
2020-04-30 17:18:31 +0200

    ProductServiceImpl service created

---
+++ src/main/java/com/company/service/ProductServieImpl.java
@@ -0,0 +1,7 @@
+package com.company.service;
+
+import com.company.api.ProductService;
+
+public class ProductServieImpl implements ProductService {
+
+}
--- src/test/java/service/ProductServiceTest.java
+++ src/test/java/service/ProductServiceTest.java
@@ -1,4 +1,19 @@
 package service;
 
+import com.company.entity.Product;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductServiceTest {
+
+    @Test
+    public void testGetProducts() {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(1L, "Czekolada", 2.80F, 0.200F, "brown", 328));
+        products.add(new Product(2L, "Coca-Cola", 4.50F, 1.750F, "black", 218));
+
+
+    }
 }
