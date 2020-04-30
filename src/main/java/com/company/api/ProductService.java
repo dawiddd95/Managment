6e288a9631ec19a702c0554269b7bb58377d3154
dawiddd95 <dawlyc@wp.pl>
2020-04-30 17:11:14 +0200

    ProductService api updated

--- src/main/java/com/company/api/ProductService.java
+++ src/main/java/com/company/api/ProductService.java
@@ -1,4 +1,13 @@
 package com.company.api;
 
+import com.company.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
+    Integer getAmountOfProducts();
+    Product getProductByName(String productName);
+    Boolean checkIfExistByName(String productName);
+    Boolean checkIfExistById(Long id);
 }
